65433fd23fde6fd9aaed60f45b8cd27fc6db8cfe
Diego Lacerda <diego.lacerda.alves@gmail.com>
2021-08-16 19:45:35 -0300

    second commit

---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        // validacao de notificacoes de erro
+
+        // validacao de modelstate
+
+        // validacao da operacao de negocios
+    }
+
+    
+
+}
